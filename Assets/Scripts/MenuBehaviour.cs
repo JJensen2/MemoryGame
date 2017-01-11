4b6e5267a71b9ab4877078bd695ce322edc73c63
Joshua Jensen <IMJJensen@gmail.com>
2017-01-11 16:52:54 -0700

    Initial Commit

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuBehaviour : MonoBehaviour {
+    public void triggerMenuBehavior(int i)
+    {
+        if(i == 1)
+        {
+            Application.Quit(); 
+        }
+        else
+        {
+            SceneManager.LoadScene("Level");
+        }
+    }
+
+}
